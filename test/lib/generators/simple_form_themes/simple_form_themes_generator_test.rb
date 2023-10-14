f1c735ab68cb809f0fe33eff770d30a350c931d0
Juan Vasquez <juan@ombulabs.com>
2023-10-14 10:55:13 -0600

    Add tailwindcss initializer

---
+++ test/lib/generators/simple_form_themes/simple_form_themes_generator_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+require "generators/simple_form_themes/simple_form_themes_generator"
+
+module SimpleFormThemes
+  class SimpleFormThemesGeneratorTest < Rails::Generators::TestCase
+    tests SimpleFormThemesGenerator
+    destination Rails.root.join("tmp/generators")
+    setup :prepare_destination
+
+    test "generator runs without errors" do
+      assert_nothing_raised do
+        run_generator ["tailwindcss"]
+      end
+    end
+
+    test "generate tailwindcss initializer" do
+      run_generator %w(tailwindcss)
+
+      assert_file "config/initializers/simple_form_tailwindcss.rb",
+        /config.default_wrapper = :vertical_form/
+    end
+  end
+end
